a362ef1b43f3c2529e968d269a90dc18bbd4a73a
kreid333 <kai.j.reid@gmail.com>
2020-09-03 17:06:37 -0400

    added all confirms

--- script.js
+++ script.js
@@ -9,18 +9,59 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Generate password function
 function generatePassword() {
   var passwordCharacterCount = prompt(
     "How many characters would you like your password to contain?"
   );
+
+  passwordCharacterCount = parseInt(passwordCharacterCount);
+
+  // Number success path
   if (passwordCharacterCount <= 128 && passwordCharacterCount >= 8) {
-    confirm("Click OK to confirm including special characters");
-  } else if (passwordCharacterCount < 8) {
+    
+
+    // Confirm list
+    var specialCharactersConfirm = confirm(
+      "Click OK to confirm including special characters"
+    );
+    var numericCharactersConfirm = confirm(
+      "Click OK to confirm including numeric characters"
+    );
+    var lowercaseCharactersConfirm = confirm(
+      "Click OK to confirm including lowercase characters"
+    );
+    var uppercaseCharactersConfirm = confirm(
+      "Click OK to confirm including uppercase characters"
+    );
+    // End: Confirm list
+
+
+    // Defining variables
+    var specialCharacterList = "~!@#$%^&*()_+{}|:\"<>?,./;'[]\\";
+    var numericCharacterList = "1234567890";
+    var lowercaseCharacterList = "abcdefghijklmnopqrstuvwxyz";
+    var uppercaseCharacterList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var emptyArray = [];
+
+    confirmArray = [specialCharactersConfirm, numericCharactersConfirm, lowercaseCharactersConfirm, uppercaseCharactersConfirm];
+    listArray = [specialCharacterList, numericCharacterList, lowercaseCharacterList, uppercaseCharacterList];
+    // End: Defining variables
+  
+  }
+  //End: Number success path
+
+
+  // Else if statements for: Number success path
+   else if (passwordCharacterCount < 8) {
     alert("Password must be at least 8 characters");
   } else if (passwordCharacterCount > 128) {
     alert("Password cannot be longer than 128 characters");
   }
+  // End
+
 }
+// End: Generate password function
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
